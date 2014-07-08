8d9336e0d28091e4bb8edd156ece592e856c06f0
vegerjiangsir <vegerjiangsir@gmail.com>
2014-07-08 16:15:27 +0800

    bjj

--- config.js
+++ config.js
@@ -28,3 +28,32 @@
     "mask":"poster/poster_mask.png"
  }
  ]
+ [
+  {
+  "url”:”1.PNG”
+  },
+  {
+  “url”:”2.PNG”
+  },
+  {
+  “url”:”3.PNG”
+  },
+  {
+  “url”:”4.PNG”
+  },
+  {
+  “url”:”5.PNG”
+  },
+  {
+  “url”:”6.PNG”
+  },
+  {
+  “url”:”7.PNG”
+  },
+  {
+  “url”:”8.PNG”
+  },
+  {
+  “url”:”9.PNG”
+  },
+  ]
