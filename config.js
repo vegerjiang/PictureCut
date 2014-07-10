e1a2e30b5868694433c31b48038f75899377b5a3
vegerjiangsir <vegerjiangsir@gmail.com>
2014-07-10 18:41:35 +0800

    asdfsad

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 {
- "appUrl":"https://itunes.apple.com/cn/app/jian-tu-shen-qi/id892067973?mt=8",
- "logoUrl":"https://raw.githubusercontent.com/vegerjiang/PictureCut/master/icon_57x57.png",
- "policyUrl":"http://user.qzone.qq.com/2661380190/blog/1403407045",
- "myMail":"jackhenizaiyiqi@qq.com"
+    "appUrl":"https://itunes.apple.com/cn/app/jian-tu-shen-qi/id892067973?mt=8",
+    "logoUrl":"https://raw.githubusercontent.com/vegerjiang/PictureCut/master/icon_57x57.png",
+    "shareAppTitle":"美女，到我的iphone里面来吧",
+    "shareAppContent":"想要剪裁并永久保存女神的照片吗？剪图神器来帮你！",
+    
+    "policyUrl":"http://user.qzone.qq.com/2661380190/blog/1403407045",
+    "myMail":"jackhenizaiyiqi@qq.com"
 }
