7a8ba655dcc56e3a7e50b1b09506fd24668b295e
vegerjiangsir <vegerjiangsir@gmail.com>
2014-07-10 18:07:37 +0800

    dsfasd

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 {
  "appUrl":"https://itunes.apple.com/cn/app/jian-tu-shen-qi/id892067973?mt=8",
+ "logoUrl":"https://raw.githubusercontent.com/vegerjiang/PictureCut/master/icon_57x57.png",
  "policyUrl":"http://user.qzone.qq.com/2661380190/blog/1403407045",
  "myMail":"jackhenizaiyiqi@qq.com"
 }
